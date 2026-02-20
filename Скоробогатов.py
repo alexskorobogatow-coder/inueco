e422144b965c2696a6927d0295575f823f251ba5
alexskorobogatow-coder <alexskorobogatow@gmail.com>
2026-02-20 11:31:59 +0500

    Скоробогатов

--- Скоробогатов.py
+++ Скоробогатов.py
@@ -1,13 +1,72 @@
-#1 print('Hello, World!')
-#2 # You know nothing, Jon Snow!
-#3 print('Robert'); print('Stannis'); print('Renly')
-#4 print('What Is Dead May Never Die')
-#5 print('What Is Dead May Never Die')
-#6 print(81 / 9)
-#7 print(6 - (-81))
-#8 print(3**5) ; print(-8/-4)
-#9 print(8 / 2 + 5 - -3 / 2)
-#10 print(70 * (3 + 4) / (8 + 2))
-#11 print(0.39 * 0.22)
-#12 print(5**2-3 * 7)
-#13 print('"Khal Drogo\'s favorite word is "athjahakar""')
+
+# 1 Привет, Мир
+print('Hello, World!')
+# 2 Комментарии
+# You know nothing, Jon Snow!
+# 3 Инструкции (Statements)
+print('Robert')
+print('Stannis')
+print('Renly')
+# 4
+print(10)
+# 5 Синтаксические ошибки
+print('What Is Dead May Never Die')
+# 6 Арифметические операции
+print(81 / 9)
+# 7 Операторы 
+print(6 - -81)
+# 
+print(3 ** 5)
+print(-8 / -4)
+# 9 Композиция операций
+print(8 / 2 + 5 - -3 / 2)
+# 10 Приоритет
+print(70 * (3 + 4) / (8 + 2))
+# 11 Числа с плавающей точкой
+print(0.39 * 0.22)
+# 12 Линтер
+print((5 ** 2) - (3 * 7))
+# 13 Кавычки
+print("\"Khal Drogo's favorite word is \"athjahakar\"\"")
+# 14 Экранированные последовательности
+print("- Did Joffrey agree?\n- He did. He also said \"I love using \\n\".")
+# 15 Конкатенация
+print('Winter ' +  'came ' + 'for ' + 'the ' + 'House ' + 'of ' + 'Frey.')
+# 16 Кодировка
+print(chr(126))
+print(chr(94))
+print(chr(37))
+# 17 Что такое переменная
+motto = 'What Is Dead May Never Die!'
+print(motto)
+# 18 Изменение переменной
+name = "Brienna"
+name = 'anneirB'
+print(name)
+# 19 Выбор имени переменной
+brother = 2
+print(brother)
+# 20 Выражения в определениях
+euros_count = 100
+d = euros_count * 1.25
+print(d)
+u = d * 6.91
+print(u)
+# 21 Переменные и конкатенация
+info = "We couldn't verify your mother's maiden name."
+intro = "Here is important information about your account security."
+first_name = "Joffrey"
+greeting = "Hello"
+print(greeting + ', ' + first_name +'!' + '\n' + intro + '\n' + info)
+# 22 Именование переменных
+x_1 = 20
+x_2 = -100
+print(x_1 * x_2)
+#
+king = "Rooms in King Balon's Castles:"
+x_6 = 6
+x_17 = 17
+print(king)
+print(x_6 * x_17)
+#
+DRAGONS_BORN_COUNT = 3
